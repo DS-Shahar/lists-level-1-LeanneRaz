a07e1cb2c45869c302b5c644f9899019d695463a
LeanneRaz <Leanne.raz.1@gmail.com>
2024-12-09 16:45:55 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,107 +1,119 @@
 import java.util.Scanner;
+
 public class Main {
 
-	public static Scanner reader = new Scanner (System.in);
-	public static void main(String[] args) {
 
-		int x = 1, y = 10, n=30;
-		int [] a = {5,6,7,8,11,15};
-		Node<Integer> head = buildListFromArr(a);
-		printListEven(head);
+		public static Scanner reader = new Scanner (System.in);
+		public static void main(String[] args) {
 
-	}
+			int x = 8, y = 10, n=30;
+			int [] a = {5,6,7,8,11,15};
+			Node<Integer> head = buildListFromArr(a);
+			numinlist(head, x);
 
-
-	public static Node<Integer> generateRandomNumbers(int x, int y, int n) {
-
-		Node<Integer> head = new Node<>((int) (Math.random() * (y - x + 1)) + x);
-		Node<Integer> current = head;
-
-		for (int i = 1; i < n; i++) {
-			Node<Integer> newNode = new Node<>((int) (Math.random() * (y - x + 1)) + x);
-			current.setNext(newNode);
-			current = current.getNext();
 		}
 
-		return head; 
-	}
 
+		public static Node<Integer> generateRandomNumbers(int x, int y, int n) {
 
-	public static int countOccurrences(Node<Integer> head, int target) {
-		int count = 0;
-		Node<Integer> current = head;
+			Node<Integer> head = new Node<>((int) (Math.random() * (y - x + 1)) + x);
+			Node<Integer> current = head;
 
-		while (current != null) {
-			if (current.getValue() == target) {
-				count++;
+			for (int i = 1; i < n; i++) {
+				Node<Integer> newNode = new Node<>((int) (Math.random() * (y - x + 1)) + x);
+				current.setNext(newNode);
+				current = current.getNext();
 			}
-			current = current.getNext();
+
+			return head; 
 		}
 
-		return count;
-	}
 
-	public static Node<Integer> buildListFromArr(int[] arr) {
-		Node<Integer> head = new Node<>(arr[0]);
-		Node<Integer> current = head;
-		for(int i=1; i<arr.length; i++) {
-			Node<Integer> newNode = new Node<>(arr[i]);
-			current.setNext(newNode);
-			current = newNode;
-		}
-		return head;
-	}
+		public static int countOccurrences(Node<Integer> head, int target) {
+			int count = 0;
+			Node<Integer> current = head;
 
-	public static void printList(Node<Integer> head) {
-		Node<Integer> current = head;
-		while (current != null) {
-			System.out.println(current.getValue());
-			current = current.getNext();
-		}
-	}
-
-	public static void printListRecursive(Node<Integer> head) {
-
-		if (head == null) {
-			return;
-		}
-		System.out.println(head.getValue());
-		printListRecursive(head.getNext());
-	}
-
-	public static void printOppositeListRecursive(Node<Integer> head) {
-
-		if (head == null) {
-			return;
-		}
-		printOppositeListRecursive(head.getNext());
-		System.out.println(head.getValue());
-
-	}	
-
-	public static Node<Integer> buildList() {
-		Node<Integer> head = new Node<Integer>(null);
-		Node<Integer> current = head;
-
-		System.out.println("Enter numbers possitive or -1");
-		int num = reader.nextInt();
-		while(num != -1) {
-			Node<Integer> newNode = new Node<Integer>(num);
-			current.setNext(newNode);
-			current = newNode;
-			System.out.println("Enter number or -1 to end:");
-			num = reader.nextInt();
-		}
-		return head.getNext();
-	}
-
-	public static void printListEven(Node<Integer> head) {
-		while(head != null) {
-			if ((head.getValue())%2==0) {
-				System.out.println(head.getValue());
+			while (current != null) {
+				if (current.getValue() == target) {
+					count++;
+				}
+				current = current.getNext();
 			}
-			head = head.getNext();
-		}
-	}
 
-}
+			return count;
+		}
+
+		public static Node<Integer> buildListFromArr(int[] arr) {
+			Node<Integer> head = new Node<>(arr[0]);
+			Node<Integer> current = head;
+			for(int i=1; i<arr.length; i++) {
+				Node<Integer> newNode = new Node<>(arr[i]);
+				current.setNext(newNode);
+				current = newNode;
+			}
+			return head;
+		}
+
+		public static void printList(Node<Integer> head) {
+			Node<Integer> current = head;
+			while (current != null) {
+				System.out.println(current.getValue());
+				current = current.getNext();
+			}
+		}
+
+		public static void printListRecursive(Node<Integer> head) {
+
+			if (head == null) {
+				return;
+			}
+			System.out.println(head.getValue());
+			printListRecursive(head.getNext());
+		}
+
+		public static void printOppositeListRecursive(Node<Integer> head) {
+
+			if (head == null) {
+				return;
+			}
+			printOppositeListRecursive(head.getNext());
+			System.out.println(head.getValue());
+
+		}	
+
+		public static Node<Integer> buildList() {
+			Node<Integer> head = new Node<Integer>(null);
+			Node<Integer> current = head;
+
+			System.out.println("Enter numbers possitive or -1");
+			int num = reader.nextInt();
+			while(num != -1) {
+				Node<Integer> newNode = new Node<Integer>(num);
+				current.setNext(newNode);
+				current = newNode;
+				System.out.println("Enter number or -1 to end:");
+				num = reader.nextInt();
+			}
+			return head.getNext();
+		}
+
+		public static void printListEven(Node<Integer> head) {
+			while(head != null) {
+				if ((head.getValue())%2==0) {
+					System.out.println(head.getValue());
+				}
+				head = head.getNext();
+			}
+		}
+		
+		public static boolean numinlist(Node<Integer> head, int x) {
+			while(head != null) {
+				if ((head.getValue())==x) {
+					return true;
+				}
+				head = head.getNext();
+			}
+			return false;
+		}
+
+	}
